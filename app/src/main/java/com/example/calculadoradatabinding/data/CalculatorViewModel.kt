ab741cdb5731e0966f5e33b4185f63d58ad682b5
Thalles Nonato <eusouthalles@gmail.com>
2022-06-10 10:08:54 -0300

    Calculadora modelo MVVM, com tratamento de erros

--- app/src/main/java/com/example/calculadoradatabinding/data/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculadoradatabinding/data/CalculatorViewModel.kt
@@ -1,7 +1,4 @@
 package com.example.calculadoradatabinding.data
-import android.view.View
-import android.widget.TextView
-import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
